2eae68f44cf82f84dcafc12b9ab091d260d6a249
MuhammadFaizan144 <fg7829098@gmail.com>
2026-03-04 21:54:00 +0500

    add basic navbar design not responsive

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
-        <nav className='flex justify-center items-center'>
-            <div className="links">
-                <div className="">
-                    <img src="" alt="" />
-                    <p>Blog Central</p>
+        <nav className='flex justify-between items-center max-w-[1440px] mx-auto px-6 h-[73px]'>
+            
+            <div className="links flex justify-center items-center gap-6">
+                <div className="flex gap-2">
+                    <div className="w-[32px] h-[32px] bg-blue-500 rounded-[4px]">
+
+                    </div>
+                    <p className='text-[23px] text-blue-500 font-bold font-merriweather'>Blog Central</p>
                 </div>
-                <ul className="">
+                <ul className="flex text-[14px] gap-6 max-sm:hidden">
                     <li><NavLink>Home</NavLink></li>
                     <li><NavLink>About</NavLink></li>
                     <li><NavLink>Blogs</NavLink></li>
@@ -18,9 +21,15 @@ const Navbar = () => {
 
                 </ul>
             </div>
-            <div className="authentication-buttons">
-                <NavLink>Login</NavLink>
-                <NavLink>Sign Up</NavLink>
+            <div className="authentication-buttons flex gap-2">
+                <div className="w-[61px] h-[36px] flex justify-center items-center">
+
+                    <NavLink className='text-[14px] '>Login</NavLink>
+                </div>
+                <div className="w-[70px] h-[36px] text-white rounded-[8px] hover:bg-white hover:text-blue-500 flex justify-center items-center bg-blue-500">
+
+                    <NavLink className='text-[14px] '>Sign Up</NavLink>
+                </div>
             </div>
         </nav>
     )
